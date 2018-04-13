d952393857487cb78b0a085d2e77717aaaf389ba
4cubo <acubo@acubo.net>
2018-04-14 00:46:32 +0200

    Funcoinando

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-﻿require('rootpath')();
+require('rootpath')();
 var express = require('express');
 var app = express();
 var cors = require('cors');
@@ -7,33 +7,34 @@ var expressJwt = require('express-jwt');
 var config = require('config.json');
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // use JWT auth to secure the api, the token can be passed in the authorization header or querystring
 app.use(expressJwt({
     secret: config.secret,
     getToken: function (req) {
-    	console.log( "Request: "+req.url );
+        console.log("Request: " + req.url);
         if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-        	console.log( "   Header JWT= "+req.headers.authorization);
+            console.log("   Header JWT= " + req.headers.authorization);
             return req.headers.authorization.split(' ')[1];
         } else if (req.query && req.query.token) {
-        	console.log( "   Param JWT= "+req.query.token);
+            console.log("   Param JWT= " + req.query.token);
             return req.query.token;
         }
         return null;
     }
-}).unless({ path: ['/users/authenticate', '/users/register'] })); //, '/uploadFile'
+}).unless({path: ['/users/authenticate', '/users/register']})); //, '/uploadFile'
 
 // routes
 app.use('/users', require('./controllers/users.controller'));
 app.use('/uploadFile', require('./controllers/uploadfile.controller'));
 
 
+
 // error handler
 app.use(function (err, req, res, next) {
-	console.log( "Error Handler: " + JSON.stringify(err) );
+    console.log("Error Handler: " + JSON.stringify(err));
     if (err.name === 'UnauthorizedError') {
         res.status(401).send('Invalid Token');
     } else {
