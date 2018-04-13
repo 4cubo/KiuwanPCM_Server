fc0e7ff244fb28955f53ed3f439d2a038d445013
4cubo <acubo@acubo.net>
2018-04-14 00:45:59 +0200

    Funcoinanado

--- controllers/uploadfile.controller.js
+++ controllers/uploadfile.controller.js
@@ -9,6 +9,7 @@ var os = require('os');
 
 //var userService = require('services/user.service');
 var uploadFileService = require('services/uploadfile.service');
+var parseFileService = require('services/parsefile.service');
 // routes
 //router.post('/authenticate', authenticate);
 //router.post('/register', register);
@@ -18,7 +19,7 @@ var uploadFileService = require('services/uploadfile.service');
 //router.delete('/:_id', _delete);
 
 router.post('/', uploadFile);
-//router.put('/:_id', uploadFile);
+router.post('/saveRequest', saveRequest);
 
 module.exports = router;
 
@@ -29,8 +30,7 @@ function uploadFile(req, res) {
 
 	form.uploadDir = os.tmpdir();
 
-	form
-		.on('file', function(field, file) {
+	form.on('file', function(field, file) {
 			//console.log(field, file);
 			//files.push([ field, file ]);
 			files.push( file );
@@ -40,7 +40,7 @@ function uploadFile(req, res) {
 			if (anyFile){ 
 			    //console.log("uploadfile.controller.uploadFile: Files OK" );
 			    uploadFileService.uploadFile( files )
-			        .then(function ( srd ) {
+			        .then(function ( srd ) { // @aaa @TODO uploadFile debe devolver error y aqui tratarse!!!
 			        	srd.fileConf.then(
 							function(result) {
 								srd.fileConf = result;
@@ -65,6 +65,29 @@ function uploadFile(req, res) {
 
 }
 
-
+function saveRequest( req, res ){
+	
+	console.log("saveRequest------------------------------------------" + JSON.stringify( req.body) );
+	parseFileService.saveRequest( req.body.obj )
+    .then(function ( saveRequestResult ) {
+//    	srd.fileConf.then(
+//			function(result) {
+//				srd.fileConf = result;
+//				console.log("	result=", srd );
+//				
+//				srd.fileIn = files;
+//				//srd.fileInfo = readExcel ();
+//				
+//				res.json( srd );
+//			}
+//    	);
+    	console.log("saveRequest result------------------------------------------" + JSON.stringify( saveRequestResult ) );
+    	res.json( saveRequestResult );
+    })
+    .catch(function (err) {
+        res.status(400).send(err);
+    });
+	
+}
 
 
